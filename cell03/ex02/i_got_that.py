b86d9d0ba382277ece454e15cd22559f084d8f85
Manarak <95946720+MANARAK-1@users.noreply.github.com>
2024-10-08 23:00:16 +0700

    Create i_got_that.py

---
+++ cell03/ex02/i_got_that.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+word = str(input("What you got to say? : "))
+a = input(("I got that! Anything else? : "))
+
+while a != "STOP" : 
+    a = input(("I got that! Anything else? : "))
+    if a == "STOP" :
+        break
