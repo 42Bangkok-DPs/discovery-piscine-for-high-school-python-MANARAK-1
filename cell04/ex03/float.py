37672a80103105bd95f19def4dd671bf3c85d715
Manarak <95946720+MANARAK-1@users.noreply.github.com>
2024-10-09 13:08:48 +0700

    Add files via upload

---
+++ cell04/ex03/float.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+number = float(input("Give me a number: "))
+
+#float(number)
+
+if number.is_integer() :
+    print("This number is an integer.")
+else :
+    print("This number is a decimal.")
