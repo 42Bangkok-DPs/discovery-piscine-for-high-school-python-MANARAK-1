92e35714d8f3d92cb7207ea53420f47e00324224
Manarak <95946720+MANARAK-1@users.noreply.github.com>
2024-10-09 11:10:01 +0700

    Add files via upload

---
+++ cell04/ex01/age.py
@@ -0,0 +1,11 @@
+age = int(input("Please tell me your age: "))
+print("You are currently", age, "years old.")
+
+#converted_num = int(age)
+
+age2 = 10
+while age2 + 10 :
+    print("In", age2, "years, you'll be", age + age2, "years old.")
+    if age2 == 30 :
+        break
+    age2 += 10
