783be8bbb2ab5c2c8761897e271a137bba4f9aca
Manarak <95946720+MANARAK-1@users.noreply.github.com>
2024-10-10 13:27:17 +0700

    Add files via upload

---
+++ cell05/ex01/play_with_arrays.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+original_array = [2, 8, 9, 48, 8, 22, -12, 2]
+new_array = []
+
+for num in original_array:
+    new_num = num + 2
+    new_array.append(new_num)
+
+print("Original array:",original_array)
+print("New array:",new_array)
